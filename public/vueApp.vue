37e8dd9caa05aea5c463edc6a7568ea94fab0e0e
Christopher Roberts <croberts4055@gmail.com>
2019-05-27 01:59:32 -0400

    Testing deployment

---
+++ public/vueApp.vue
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+<template>
+  <section id="SmallBusiness">
+    <feed>
+      <slot />
+    </feed>
+  </section>
+</template>
+<script>
+const url = "https://data.cityofnewyork.us/resource/ci93-uc8s.json";
+
+const vm = new Vue({
+        el: '#app',
+        data: {
+          results: []
+        },
+        mounted() {
+          axios.get(url).then(response => {
+            this.results = response.data
+          })
+        }
+      });
+</script>
